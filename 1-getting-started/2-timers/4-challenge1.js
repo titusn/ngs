ae869fa5202e39a3c9e1ac37b124b0330c2cc3ca
Titus Nachbauer <titus.nachbauer@xillio.com>
2019-06-03 17:05:30 +0200

    day 1

--- 1-getting-started/2-timers/4-challenge1.js
+++ 1-getting-started/2-timers/4-challenge1.js
@@ -1,9 +1,13 @@
-const theOneFunc = () => {};
-
-setTimeout(theOneFunc, 4 * 1000);
+const theOneFunc = timeout => {
+    console.log("Hello after " + timeout/1000 + " seconds"); 
+};
 
 // Hello after 4 seconds
+let timeout = 4000;
+setTimeout(theOneFunc, timeout, timeout);
 
 // Hello after 8 seconds
+timeout = 8000;
+setTimeout(theOneFunc, timeout, timeout);
 
 // You can define only ONE function
--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -3,3 +3,17 @@
 // And stop after 5 times
 
 // After 5 times. Print "Done" and let Node exit.
+
+let counter = 0;
+
+const HelloWorld = () => {
+    counter++;
+    if (counter <= 5) {
+        console.log("Hello World!");
+    } else {
+        console.log("Done");
+        clearInterval(intervalId);
+    }
+}
+
+let intervalId = setInterval(HelloWorld, 1000);
